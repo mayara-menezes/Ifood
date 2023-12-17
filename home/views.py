46ae98d628b21cbf63d0ec154a5d024ca8cf7956
mayara-menezes <79277369+mayara-menezes@users.noreply.github.com>
2023-12-16 23:23:34 -0300

    Refactor code for improved performance and readability

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    context = {}
+    
+    html_template = loader.get_template('home/index.html')
+    
+    return HttpResponse(html_template.render(context, request))
